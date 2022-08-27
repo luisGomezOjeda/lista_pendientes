8599aca52c26d3524a959917e550dfa257f0a0a4
luis gomez <luisgomezojeda110502@gmail.com>
2022-08-26 23:26:09 -0300

    setting fixes

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,10 @@ import {local_storage,theme_selector} from "./theme.js";
 import {list_chores,load_list_chores,create_li,load_todo,create_todo} from "./create_delete--li.js"
 
 
-let settings_items__counter = list_chores.length;
-
-let settings_items__counter_true = list_chores.filter((item)=>item.status === true);
-
-let items__counter = document.querySelector(".settings-items__counter").innerHTML =`${settings_items__counter}/${settings_items__counter_true.length}`;
+let settings_items__counter = list_chores.length,
+settings_items__counter_true = list_chores.filter((item)=>item.status === true),
+items__counter = document.querySelector(".settings-items__counter").innerHTML =`${settings_items__counter}/${settings_items__counter_true.length}`,
+$li = document.querySelectorAll(".list-ul-todo-li");
 
 function load_counter(){
  settings_items__counter = document.querySelectorAll(".list-ul-todo li").length;
@@ -26,23 +25,46 @@ window.addEventListener("click",(e)=>{
  if(e.target.matches(".list-ul-todo-li button"))load_counter();
 
  if(e.target.matches("#status__all")){
-  status__completed.checked = false;
-   status__earring.checked = false;
+   if(status__all.checked){
+      status__completed.checked = false;
+      status__earring.checked = false;
+      $li.forEach(li=>{
+        li.style.setProperty("display","flex");
+      });
+    }
+
   if(!(status__earring.checked || status__completed.checked)){
    status__all.checked = true;
   }
  }
   if(e.target.matches("#status__completed")){
-   status__all.checked = false;
-   status__earring.checked = false;
-   if(!(status__all.checked || status__completed.checked)){
+    if(status__completed.checked){
+      status__all.checked = false;
+      status__earring.checked = false;
+
+      list_chores.forEach((li,index)=>{
+        $li[index].style.setProperty("display","flex");
+        if(!li.status){
+          $li[index].style.setProperty("display","none");
+        }
+      });
+    }
+    if(!(status__all.checked || status__earring.checked)){
     status__completed.checked = true;
    }
   }
   if(e.target.matches("#status__earring")){
    status__all.checked = false;
    status__completed.checked = false;
-   if(!(status__all.checked || status__status__earring.checked)){
+
+    list_chores.forEach((li,index)=>{
+      $li[index].style.setProperty("display","flex");
+      if(li.status){
+        $li[index].style.setProperty("display","none");
+      }
+    });
+
+   if(!(status__all.checked || status__completed.checked)){
     status__earring.checked = true;
    }
   }
